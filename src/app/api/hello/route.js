9bb1129eadfa25f78f8d6f02e8e14ef4acd50020
ismael strey pereira <ismaelstrey@hotmail.com>
2023-04-28 15:41:27 -0300

    Update API

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,3 +1,10 @@
+import { NextResponse } from "next/server"
 export async function GET(request) {
-  return new Response('Hello, Next.js!')
+  return NextResponse.json({ message: 'Hello, Next.js!' })
+}
+
+export async function POST(request) {
+  const res = await request.json()
+  console.log(res)
+  return NextResponse.json({ res })
 }
